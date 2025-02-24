19cb7808990fef5de8f1df140fbbb3d86e76bcc2
K1MHJ <hojokimu@gmail.com>
2025-02-24 12:24:23 +0900

    사용하지 않는 변수가 있을 때 빌드 에러가 발생하는 막기 위한 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // 사용되지 않는 변수 에러 비활성화
+    },
+  },
 ];
 
 export default eslintConfig;
