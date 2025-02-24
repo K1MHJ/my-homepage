624da5ce545b905dc71d1caa8cb11946d4fba52e
K1MHJ <hojokimu@gmail.com>
2025-02-24 16:40:30 +0900

    스타일 정의를 재이용

--- src/pages/StyleSample.tsx
+++ src/pages/StyleSample.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const H1 = styled.h1`
   color: red;
@@ -12,6 +12,32 @@ type ButtonProps = {
   color: string
   backgroundColor: string
 }
+
+//빨간색 경계선을 표시하는 스타일
+const redBox = css`
+padding:0.25em 1em;
+border:3px solid #ff0000;
+border-radius:10px;
+`
+//파란색을 표시하는 스타일
+const font = css`
+color:#1e90ff;
+font-size:2em;
+`
+
+//빨간색 경계선과 파란색 스타일을 각각 적용하고, 배경이 투명한 버튼 콤포먼트
+const Button2 = styled.button`
+background: transparent;
+margin: 1em;
+cursor: pointer;
+${redBox}
+${font}
+`
+//파란색 글자의 스타일을 계승하고, 두꺼운 글자를 표시하는 컴포먼트
+const Text = styled.p`
+font-weight:bold;
+${font}
+`
 
 const Button = styled.button<ButtonProps>`
 color: ${(props) => props.color};
@@ -39,6 +65,11 @@ const StyleSample: NextPage = (props) => {
         <Button backgroundColor="#1E90FF" color="white">
           World
         </Button>
+      </div>
+      <div>
+        {/*파란문자로 빨간색 경계선 버튼을 표시*/}
+        <Button2>Hello</Button2>
+        <Text>World</Text>
       </div>
     </div>
   )
